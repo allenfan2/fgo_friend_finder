6220958b23d4c964166b1bdcf8af0f32b3b78db7
Allen Fan <allenfan@live.com>
2019-03-21 23:50:57 -0400

    Registration/Login System implemented

--- fgo_friend_finder/settings.py
+++ fgo_friend_finder/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'matcher.apps.MatcherConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = "matcher-home"
+
+LOGIN_URL = "login"
